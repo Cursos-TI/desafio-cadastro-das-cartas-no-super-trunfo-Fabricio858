7738a37e3071ab2b6562501927268b34c1196e75
fabricio <fabriciodamorim@gmail.com>
2025-03-13 21:00:10 +0000

    desafio finalizado

---
+++ supertrunfo.c/trunfo.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+int main(){
+  char carta [50];
+  char estado [50]; 
+  char codigo [50];
+  char cidade [50];
+  int populacao;
+  int pontos;
+  float area;
+  float pib;
+  //printf imprime o que o usuario deve colocar, scanf para ler a entrada do usuario
+  printf("Digite O Número Da Carta: \n");
+  scanf("%s", &carta);
+
+  printf("Digite O Nome Estado: \n");
+  scanf("%s", &estado);
+
+  printf("Digite O Número Do Código: \n");
+  scanf("%s", &codigo);
+
+  printf("Digite O Nome Da Cidade: \n");
+  scanf("%s", &cidade);
+// printf pede para o usuario inserir um valor, scanf retorna o valor inteiro que o usuario colocar
+printf("Digite O Número De Habitantes: \n");
+scanf("%d", &populacao);
+//printf pede para o usuario inserir um valor, scanf retorna um numero com ponto flutuante
+printf("Digite O Tamanho Da Àrea: \n");
+scanf("%f", &area);
+
+printf("Digite O Pib Da Cidade: \n");
+scanf("%f", &pib);
+
+printf("Digite O Número De Pontos Turisticos: \n");
+scanf("%d",&pontos);
+
+// aqui o printf retorna todos os valores que o usuario inseriu anteriormente
+  printf("Carta: %s \n",  carta);
+  printf("Estado: %s \n", estado);
+  printf("Codigo: %s \n", codigo);
+  printf("Nome da Cidade: %s \n", cidade);
+  printf("População: %d \n", populacao);
+  printf("Àrea: %f km² \n",area);
+  printf("PIB: %.3f Bilhões de Reais \n",pib);
+  printf("Número de Pontos Turisticos: %d \n", pontos);
+
+
+  
+  //printf imprime o que o usuario deve colocar, scanf para ler a entrada do usuario
+  printf("Digite O Número Da Carta: \n");
+  scanf("%s", &carta);
+
+  printf("Digite O Nome Estado: \n");
+  scanf("%s", &estado);
+
+  printf("Digite O Número Do Código: \n");
+  scanf("%s", &codigo);
+
+  printf("Digite O Nome Da Cidade: \n");
+  scanf("%s", &cidade);
+// printf pede para o usuario inserir um valor, scanf retorna o valor inteiro que o usuario colocar
+printf("Digite O Número De Habitantes: \n");
+scanf("%d", &populacao);
+//printf pede para o usuario inserir um valor, scanf retorna um numero com ponto flutuante
+printf("Digite O Tamanho Da Àrea: \n");
+scanf("%f", &area);
+
+printf("Digite O Pib Da Cidade: \n");
+scanf("%f", &pib);
+
+printf("Digite O Número De Pontos Turisticos: \n");
+scanf("%d",&pontos);
+
+  //printf imprime o que o usuario deve colocar, scanf para ler a entrada do usuario
+  printf("Carta: %s \n",  carta);
+  printf("Estado: %s \n", estado);
+  printf("Codigo: %s \n", codigo);
+  printf("Nome da Cidade: %s \n", cidade);
+  printf("População: %d \n", populacao);
+  printf("Àrea: %f km² \n",area);
+  printf("PIB: %.3f Bilhões de Reais \n",pib);
+  printf("Número de Pontos Turisticos: %d \n", pontos);
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
